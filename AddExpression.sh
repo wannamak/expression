e15783d3df2cd2e1e2fd8e3d7c6e83a50633ce82
Keith Wannamaker <keith@wannamaker.org>
2020-03-20 20:16:46 -0400

    Compile before running

--- AddExpression.sh
+++ AddExpression.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -cp bin:lib/guava-21.0.jar:lib/mysql.jar transform.AddExpression $@
+javac -cp lib/guava-21.0.jar:lib/mysql.jar -sourcepath src -d bin src/transform/AddExpression.java
+if [ $? = 0 ]; then
+  java -cp bin:lib/guava-21.0.jar:lib/mysql.jar transform.AddExpression $@
+fi
